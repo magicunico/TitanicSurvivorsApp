fb0d3e7a6ae64620dbffc7d2be37ec1924d3d78a
Jula13 <jula7092@gmail.com>
2021-03-16 16:43:15 +0100

    fix

--- titanic-front/src/App.js
+++ titanic-front/src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import './App.css';
-import Tabs from "./components/Tabs"
 import Passengers from './Passengers'
 
 function App() {
   return (
-   <Tabs> 
-     <div label="Passengers data"> 
-       <Passengers/>
-     </div>      
-   </Tabs> 
+  <Passengers/>
   );
 }
 
